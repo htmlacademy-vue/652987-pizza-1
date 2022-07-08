0b6c077ef441a200e965cdfa073b1fc6ddafb7c1
t.mityagina <394999325@mail.ru>
2022-07-08 12:23:49 +0300

    2.6. Разбивка компонента «Конструктор пиццы» на мелкие компоненты

---
+++ src/frontend/src/common/components/RadioButton.vue
@@ -0,0 +1,42 @@
+<template>
+  <label :class="classRadioLabel">
+    <input
+      type="radio"
+      :name="name"
+      :value="value"
+      :class="classRadioInput"
+      @change="$emit('change', $event.target.value)"
+    />
+    <slot></slot>
+  </label>
+</template>
+
+<script>
+export default {
+  name: "RadioButton",
+  model: {
+    prop: "checked",
+    event: "change",
+  },
+  props: {
+    classRadioLabel: {
+      type: String,
+      required: false,
+    },
+    classRadioInput: {
+      type: String,
+      required: false,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    value: {
+      type: String,
+      required: true,
+    },
+  }
+}
+</script>
+
+<style lang="scss" scoped></style>
